05b47797563f2deece8f82ecee16c62226a7792b
PRADEEPKUMARJEYAKUMAR <51090588+PRADEEPKUMARJEYAKUMAR@users.noreply.github.com>
2019-08-01 18:27:26 +0530

    Update LCM.py

--- LCM.py
+++ LCM.py
@@ -1,19 +1,11 @@
 p,b=map(int,input().split())
-l=[]
-l1=[]
-for i in range(1,p):
-    if(p%i==0):
-        l.append(i)
-for i in range(1,b):
-    if(b%i==0):
-        l1.append(i)
-q=[]
-for i in l:
-        q.append(i)
-for i in l1:
-    q.append(i)
-w=1
-#print(l,l1,q)
-for i in q:
-    w*=int(i)
-print(w)
+if(p>b):
+    big=p
+else:
+    big=b
+while(True):
+    if(big%p==0 and big%b==0):
+        lcm=big
+        break
+    big+=1
+print(lcm)
